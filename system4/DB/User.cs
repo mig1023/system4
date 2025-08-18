d64c9c734290a91d3ecc5a11d97e31a3fd4df3e7
mig1023 <mig1023@mail.ru>
2025-08-18 10:49:22 +0300

    users entity

--- system4/DB/User.cs
+++ system4/DB/User.cs
@@ -13,10 +13,42 @@ namespace system4.DB
 
         public string Email { get; set; }
 
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime StatusDate { get; set; }
+
+        public DateTime PassChanged { get; set; }
+
+        public int RoleID { get; set; }
+
+        public int Attempts { get; set; }
+
         public string UserLName { get; set; }
 
         public string UserName { get; set; }
 
         public string UserSName { get; set; }
+
+        public int Gender { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public int Locked { get; set; }
+
+        public DateTime LockDate { get; set; }
+
+        public string LockedBy{ get; set; }
+
+        public string LockReason { get; set; }
+
+        public string Branches { get; set; }
+
+        public string LangID { get; set; }
+
+        public string Printer { get; set; }
+
+        public string Cashbox { get; set; }
+
+        public string CompanyID { get; set; }
     }
 }
