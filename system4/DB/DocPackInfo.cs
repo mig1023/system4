a40451625ddea7376285e0d552e57ad4f35e0c9c
mig1023 <mig1023@mail.ru>
2025-08-24 09:43:19 +0300

    docpackinfo db

---
+++ system4/DB/DocPackInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace system4.DB
+{
+    public class DocPackInfo
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int PackID { get; set; }
+
+        public string BankID { get; set; }
+
+        public float PSum { get; set; }
+
+        public int VisaCnt { get; set; }
+
+        public int Num_NR { get; set; }
+
+        public int Num_NC { get; set; }
+
+        public int Num_NN { get; set; }
+
+        public string WhomFilled { get; set; }
+
+        public DateTime WhenFilled { get; set; }
+
+        public int Num_ACon { get; set; }
+
+        public int Num_ANCon { get; set; }
+    }
+}
