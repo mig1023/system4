c81ab760ba3bdc20a1b6d0617b1f4a495b58520a
mig1023 <mig1023@mail.ru>
2025-08-28 12:44:50 +0300

    app search with sessions

--- system4/Pages/Docs/Apps.cshtml.cs
+++ system4/Pages/Docs/Apps.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using system4.DB;
 
 namespace system4.Pages.Docs
 {
     [Authorize]
     public class AppsModel : PageModel
     {
+        [BindProperty]
+        public string Search { get; set; }
+
         public List<DAL.Appointment> Appointments { get; set; }
 
         public int Pages { get; set; }
@@ -14,10 +18,35 @@ namespace system4.Pages.Docs
         public int Current { get; set; }
 
         public void OnGet(int? pageNum)
-        {       
-            Appointments = DAL.Appointment.List(DateTime.Now.ToString(), pageNum, out int pages);
+        {
+            var search = HttpContext.Session.GetString("appSearch");
+
+            if (String.IsNullOrEmpty(search))
+            {
+                search = DateTime.Now.ToString();
+            }
+            else
+            {
+                Search = search;
+            }
+
+            Appointments = DAL.Appointment.List(search, pageNum, out int pages);
             Pages = pages;
             Current = pageNum ?? 1;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Request.Form.Keys.Contains("doSearch"))
+            {
+                HttpContext.Session.SetString("appSearch", Search);
+            }
+            else if (Request.Form.Keys.Contains("doClean"))
+            {
+                HttpContext.Session.Remove("appSearch");
+            }
+            
+            return RedirectToPage();
         }
     }
 }
