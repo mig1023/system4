da8a9b20a79b958ff668709b3f5fe2e33feed702
mig1023 <mig1023@mail.ru>
2025-08-30 11:01:40 +0300

    history search with session

--- system4/Pages/Docs/History.cshtml.cs
+++ system4/Pages/Docs/History.cshtml.cs
@@ -10,21 +10,33 @@ namespace system4.Pages.Docs
 
         public List<DAL.DocHistory> History { get; set; }
 
-        public void OnGet(string? searchNum)
+        public void OnGet()
         {   
-            if (!string.IsNullOrEmpty(searchNum))
+            var search = HttpContext.Session.GetString("historySearch");
+
+            if (String.IsNullOrEmpty(search))
             {
-                History = DAL.DocHistory.List(searchNum);
+                History = new List<DAL.DocHistory>();
             }
             else
             {
-                History = new List<DAL.DocHistory>();
+                History = DAL.DocHistory.List(search);
+                Search = search;
             }
         }
 
         public IActionResult OnPost()
         {
-            return Redirect($"/history/{Search}");
+            if (Request.Form.Keys.Contains("doSearch"))
+            {
+                HttpContext.Session.SetString("historySearch", Search);
+            }
+            else if (Request.Form.Keys.Contains("doClean"))
+            {
+                HttpContext.Session.Remove("historySearch");
+            }
+
+            return RedirectToPage();
         }
     }
 }
