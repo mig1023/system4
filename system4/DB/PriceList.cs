4b99184f0d5e1ad50d806a6d6a7bc1d71c9b0d22
mig1023 <mig1023@mail.ru>
2025-08-30 11:42:25 +0300

    pricelist db

---
+++ system4/DB/PriceList.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace system4.DB
+{
+    public class PriceList
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int RateId { get; set; }
+
+        public float Price { get; set; }
+
+        public float UPrice { get; set; }
+
+        public float JPrice { get; set; }
+
+        public float JUPrice { get; set; }
+
+        public int VisaId { get; set; }
+
+        public float ConcilR { get; set; }
+
+        public float ConcilN { get; set; }
+
+        public int woVAT { get; set; }
+
+        public float ConcilRU { get; set; }
+
+        public float ConcilNU { get; set; }
+
+        public string Ages { get; set; }
+    }
+}
