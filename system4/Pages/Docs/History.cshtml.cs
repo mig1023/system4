756231af959eff5b6b319190a76568f1a6691e21
mig1023 <mig1023@mail.ru>
2025-09-01 14:51:10 +0300

    authorize fix

--- system4/Pages/Docs/History.cshtml.cs
+++ system4/Pages/Docs/History.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace system4.Pages.Docs
 {
+    [Authorize]
     public class HistoryModel : PageModel
     {
         [BindProperty]
