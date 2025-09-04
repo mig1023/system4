0a2d6bdd54c88a16820e3401b08e7538b3f30718
mig1023 <mig1023@mail.ru>
2025-09-04 14:58:22 +0300

    finances.service class

---
+++ system4/BLL/Finances/Service.cs
@@ -0,0 +1,17 @@
+﻿namespace system4.BLL.Finances
+{
+    public class Service
+    {
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+
+        public bool VAT { get; set; }
+
+        public int Departament { get; set; }
+
+        public string Code { get; set; }
+    }
+}
