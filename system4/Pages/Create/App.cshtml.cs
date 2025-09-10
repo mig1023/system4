9c12b03001396cff057112a56def091316842b09
mig1023 <mig1023@mail.ru>
2025-09-10 16:30:05 +0300

    app create start

---
+++ system4/Pages/Create/App.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace system4.Pages.Create
+{
+    [Authorize]
+    public class AppModel : PageModel
+    {
+        public Dictionary<int, string> Centers { get; set; }
+
+        public void OnGet()
+        {
+            Centers = DB.Entity.Get.Branches()
+                .ToDictionary(x => x.Id, x => x.BName);
+        }
+    }
+}
