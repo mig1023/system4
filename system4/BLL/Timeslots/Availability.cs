9ad016dfa41a403820120397c50016fa4e901d7f
mig1023 <mig1023@mail.ru>
2025-09-23 15:17:04 +0300

    timeslot availability

---
+++ system4/BLL/Timeslots/Availability.cs
@@ -0,0 +1,11 @@
+﻿namespace system4.BLL.Timeslots
+{
+    public class Availability
+    {
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public Dictionary<string, string> Dates { get; set; }
+    }
+}
