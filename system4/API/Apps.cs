fe4f6782b7281083a4876ab614a65405f6232cec
mig1023 <mig1023@mail.ru>
2025-09-24 17:00:14 +0300

    api (start)

---
+++ system4/API/Apps.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace system4.API
+{
+    [Authorize]
+    [ApiController]
+    public class Apps : ControllerBase
+    {
+        [HttpGet("api/visatypes/{centerId}")]
+        public ActionResult<List<DB.VisaTypes>> VisaTypes(int centerId)
+        {
+            return DB.Entity.Get.VisaTypesByCenter(centerId);
+        }
+    }
+}
--- system4/Program.cs
+++ system4/Program.cs
@@ -26,6 +26,10 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseEndpoints(endpoints => {
+    endpoints.MapControllers();
+});
+
 app.MapRazorPages();
 
 app.Run();
