dbc53065a8e316ccca472ca2ee753ca511ce349e
mig1023 <mig1023@mail.ru>
2025-10-07 14:37:24 +0300

    doc create start

--- system4/Pages/Create/Doc.cshtml.cs
+++ system4/Pages/Create/Doc.cshtml.cs
@@ -1,12 +1,19 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace system4.Pages.Create
 {
     public class DocModel : PageModel
     {
-        public void OnGet()
+        public DAL.Appointment Appointment { get; set; }
+
+        public Dictionary<int, string> VisaTypes { get; set; }
+
+        public void OnGet(int appid)
         {
+            Appointment = DAL.Appointment.Get(appid);
+
+            VisaTypes = DB.Entity.Get.VisaTypesByCenter(Appointment.CenterId)
+                .ToDictionary(x => x.Id, x => x.VName);
         }
     }
 }
