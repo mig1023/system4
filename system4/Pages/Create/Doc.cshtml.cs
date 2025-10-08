5d76939bd2251eff33beacf9a03893a6fe707a24
mig1023 <mig1023@mail.ru>
2025-10-08 12:09:24 +0300

    BindProperty + id/name fixes

--- system4/Pages/Create/Doc.cshtml.cs
+++ system4/Pages/Create/Doc.cshtml.cs
@@ -1,12 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace system4.Pages.Create
 {
+    [Authorize]
     public class DocModel : PageModel
     {
         public DAL.Appointment Appointment { get; set; }
 
         public Dictionary<int, string> VisaTypes { get; set; }
+
+        [BindProperty]
+        public BLL.CreateDoc.DocForm DocPack { get; set; }
 
         public void OnGet(int appid)
         {
@@ -14,6 +20,11 @@ namespace system4.Pages.Create
 
             VisaTypes = DB.Entity.Get.VisaTypesByCenter(Appointment.CenterId)
                 .ToDictionary(x => x.Id, x => x.VName);
+        }
+
+        public IActionResult OnPost()
+        {
+            return null;
         }
     }
 }
