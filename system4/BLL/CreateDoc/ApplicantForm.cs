601dcac764771e78268c29c49f78f1c0fb5882b3
mig1023 <mig1023@mail.ru>
2025-10-08 11:23:56 +0300

    doc create ApplicantForm

---
+++ system4/BLL/CreateDoc/ApplicantForm.cs
@@ -0,0 +1,25 @@
+﻿namespace system4.BLL.CreateDoc
+{
+    public class ApplicantForm
+    {
+        public bool Removed { get; set; }
+
+        public string RLName { get; set; }
+
+        public string RFName { get; set; }
+
+        public string RMName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public DateTime FlyDate { get; set; }
+
+        public string BankId { get; set; }
+
+        public int Request { get; set; }
+
+        public bool Concil { get; set; }
+
+        public bool NRes { get; set; }
+    }
+}
