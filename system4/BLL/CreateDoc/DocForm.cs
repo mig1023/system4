0ef35f0931af52b53705788b4f544dc5a7ff987e
mig1023 <mig1023@mail.ru>
2025-10-11 17:08:09 +0300

    DocPerson "radio" checkbox

--- system4/BLL/CreateDoc/DocForm.cs
+++ system4/BLL/CreateDoc/DocForm.cs
@@ -10,6 +10,10 @@
 
         public ApplicantForm ApplicantDwhom { get; set; }
 
+        public string LName { get; set; }
+        public string FName { get; set; }
+        public string MName { get; set; }
+
         public DocForm()
         {
             Applicants = new List<ApplicantForm>();
