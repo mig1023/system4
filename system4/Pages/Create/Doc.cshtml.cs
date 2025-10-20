e06bfc5cc3298000b0d2276d776dea30ad773072
mig1023 <mig1023@mail.ru>
2025-10-20 15:44:09 +0300

    coding page fix

--- system4/Pages/Create/Doc.cshtml.cs
+++ system4/Pages/Create/Doc.cshtml.cs
@@ -80,7 +80,7 @@ namespace system4.Pages.Create
                     }
                     else
                     {
-                        ModelState.AddModelError($"DocPack.Services[{servicesIndex}]", "Значение услуги не задано");
+                        ModelState.AddModelError($"DocPack.Services[{servicesIndex}]", "Р—РЅР°С‡РµРЅРёРµ СѓСЃР»СѓРіРё РЅРµ Р·Р°РґР°РЅРѕ");
                     }
                 }
 
