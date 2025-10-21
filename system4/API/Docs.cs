5dce258537890f9c43670f0baeda047cc963fba4
mig1023 <mig1023@mail.ru>
2025-10-21 14:20:11 +0300

    dadata integration api

---
+++ system4/API/Docs.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace system4.API
+{
+    [Authorize]
+    [ApiController]
+    public class Docs : ControllerBase
+    {
+        [HttpGet("api/dadata/{address}")]
+        public async Task<string> Dadata(string address)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+                    client.DefaultRequestHeaders.Add("Authorization", $"Token {Secret.Dadata["Token"]}");
+
+                    HttpResponseMessage response = await client.PostAsJsonAsync(Secret.Dadata["Url"], new { query = address });
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return string.Empty;
+                    }
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (Exception)
+                {
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ system4/API/Secret.cs
@@ -0,0 +1,11 @@
+﻿namespace system4.API
+{
+    public class Secret
+    {
+        public static Dictionary<string, string> Dadata = new Dictionary<string, string>
+        {
+            ["Url"] = "127.0.0.1",
+            ["Token"] = "",
+        };
+    }
+}
